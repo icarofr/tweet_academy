0bcc18c25a06dfd349a9608c5162f77f57c9102c
Rebecca <rebecca.andriamanantena@epitech.eu>
2020-02-10 17:02:47 +0100

    tweet_academie

---
+++ Rebecca/tweet_academie.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Tweet_academie</title>
+        <link rel="stylesheet" href="style.css"/>
+    </head>
+        <body>
+            <div class="logo">
+                <img src="logo.png" alt="logo">
+            </div>
+            <div class="menu">
+                <ul>
+                    <li><a href="">Accueil</a></li>
+                    <li><a href="">Mon compte</a>
+                    <li><a href="">Notifications</a></li>
+                    <li><a href="">Messages</a></li>
+                    <li><a href="">Listes</a></li>
+                    <li><a href="">Profil</a></li>
+                    <li><a href="">Plus</a></li>
+                </ul>
+                <input class="tweet" type="submit" name="tweet"value="TWEET"/>
+            </div>
+
+            <section>
+                    <div class="container1">
+                    <div>Accueil</div>
+                    <div class="textarea" name="textarea" id="textfield"></div>
+                </div>
+            </section>
+        </body>
+</html>
