9dc4ba06334e64253945d65271d92f28963b81ec
Rebecca <rebecca.andriamanantena@epitech.eu>
2020-02-11 15:50:54 +0100

    my_meetic

---
+++ Rebecca/page_message.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Tweet_academie</title>
+        <link rel="stylesheet" href="style.css"/>
+        <link href="https://fonts.googleapis.com/css?family=Manjari&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Courier+Prime&display=swap" rel="stylesheet">
+    </head>
+        <body>
+            <div class="logo">
+                <img src="logo.png" alt="logo">
+            </div>
+            <div class="menu">
+                <ul>
+                    <li><a href="">Accueil</a></li>
+                    <li><a href="">Mon compte</a>
+                    <li><a href="">Notifications</a></li>
+                    <li><a href="">Messages</a></li>
+                    <li><a href="">Listes</a></li>
+                    <li><a href="">Profil</a></li>
+                    <li><a href="">Plus</a></li>
+                </ul>
+                <input class="tweet" type="submit" name="tweet"value="TWEET"/>
+            </div>
