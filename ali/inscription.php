e215b31f2eec3d6071fddd63db8a476d795bab96
Ali <ali.ouaibi@epitech.eu>
2020-02-11 15:52:32 +0100

    inscription html fini

--- ali/inscription.php
+++ ali/inscription.php
@@ -7,17 +7,14 @@ session_start();
 
 //     $Nom = htmlspecialchars($_POST['nom']);
 //     $Prenom = htmlspecialchars($_POST['prenom']);
+//     $Pseudo = htmlspecialchars($_POST['pseudo']);
 //     $Date = $_POST['date'];
-//     $Sexe = $_POST['sexe'];
-//     $Ville = $_POST['ville'];
 //     $Mail = htmlspecialchars($_POST['mail']);
 //     $Mail2 = htmlspecialchars($_POST['mail2']);
-//     $Mdp = sha1($_POST['mdp']);
-//     $Mdp2 = sha1($_POST['mdp2']);
-//     $Age = date("Y") - substr($_POST['date'], 0, 4);
-//     $loisir = htmlspecialchars($_POST['loisir']);
+//     $Mdp = hash('ripemd160', $_POST['mdp']);
+//     $Mdp2 = hash('ripemd160', $_POST['mdp2']);
     
-//     if (!empty($_POST['nom']) AND !empty($_POST['prenom']) AND !empty($_POST['date']) AND !empty($_POST['sexe']) AND !empty($_POST['ville']) AND !empty($_POST['mail']) AND !empty($_POST['mail2']) AND !empty($_POST['mdp']) AND !empty($_POST['mdp2']) AND !empty($_POST['loisir'])) 
+//     if (!empty($_POST['nom']) AND !empty($_POST['prenom']) AND !empty($_POST['date']) AND !empty($_POST['pseudo']) AND !empty($_POST['mail']) AND !empty($_POST['mail2']) AND !empty($_POST['mdp']) AND !empty($_POST['mdp2'])) 
 //     {    
 //         if ($Nom !='' && $Prenom !="" && $Date !="" && $Sexe!="" && $Ville !="") 
 //         {
@@ -71,10 +68,10 @@ session_start();
 <div id="contenu">
     <div id="inscription">
   
-    <h1 class="t_inscription">INSCRIPTION</h1>
+    <h1 class="t_inscription">INSCR<span class="color">IPTION</span></h1>
 
     <form method="POST" action="#">
-    <table>
+    <table class="form_inscription">
         <tr>
             <td align="right"><label for="nom">Nom :</label></td>
             <td><input type="text" placeholder="Votre nom" name="nom"></td>
@@ -86,30 +83,13 @@ session_start();
         </tr>
 
         <tr>
+            <td align="right"><label for="pseudo">Pseudo :</label></td>
+            <td><input type="text" placeholder="Votre pseudo" name="pseudo"></td>
+        </tr>
+
+        <tr>
             <td align="right"><label for="date">Date de naissance :</label></td>
             <td><input type="date" class="start" name="date" min="1960-01-01" max="2020-12-31"></td>
-        </tr>
-
-        <tr>
-            <td align="right"><label for="sexe">Sexe :</label></td>
-                <td><input type="radio" name="sexe" value="homme" id="sexe" required>Homme</input>
-                <input type="radio" name="sexe" value="femme" id="sexe" required>Femme</input>
-                <input type="radio" name="sexe" value="autre" id="sexe" required>Autre</input></td>
-        </tr>
-
-        <tr>
-            <td align="right"><label for="ville">Choisir votre ville :</label></td>
-            <td><select name="ville">
-                <option selected>Ville</option>
-                <option value='Paris'>Paris</option>
-                <option value='Marseille'>Marseille</option>
-                <option value='Lyon'>Lyon</option>
-                <option value='Nice'>Nice</option>
-                <option value='Lille'>Lille</option>
-                <option value='Nantes'>Nantes</option>
-                <option value='Bordeaux'>Bordeaux</option>
-                <option value='Toulouse'>Toulouse</option>
-            </select></td>
         </tr>
 
         <tr>
@@ -131,20 +111,6 @@ session_start();
             <td align="right"><label for="mdp2">Confirmation du mot de passe :</label></td>
             <td><input type="password" placeholder="Votre mot de passe" name="mdp2"></td>
         </tr>
-        <tr>
-            <td align="right"><label for="loisir">Choisir un loisir :</label></td>
-            <td><select name="loisir">
-            <option selected>Loisir</option>
-                <option value='cuisine'>cuisine</option>
-                <option value='cinema'>cinema</option>
-                <option value='sport'>sport</option>
-                <option value='lire'>lire</option>
-                <option value='jeux video'>jeux video</option>
-                <option value='shooping'>shooping</option>
-                <option value='musique'>musique</option>
-                <option value='restaurant'>restaurant</option>
-            </select></td>
-        </tr>
     </table><br><br>
 
     <button type="submit" name="forminscription" class="submit_inscription">Valider</button>
@@ -154,9 +120,6 @@ session_start();
     
     </div>
 
-    <div class="amour">
-        <img src="image/text.png" alt="" name="text" />
-    </div>
 
 </div>
 </main>
