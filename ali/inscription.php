c8eb742b945d72702d72110c85365806293bc5d2
Meryem Ben Jouda <meryem.ben-jouda@epitech.eu>
2020-02-11 16:37:08 +0100

    html terminé

--- ali/inscription.php
+++ ali/inscription.php
@@ -67,7 +67,7 @@ session_start();
     </div>
 <div id="contenu">
     <div id="inscription">
-  
+        
     <h1 class="t_inscription">INSCR<span class="color">IPTION</span></h1>
 
     <form method="POST" action="#">
