67a77b48f627f652086972224a4a9637260f7276
Ali <ali.ouaibi@epitech.eu>
2020-02-19 15:58:10 +0100

    tweet profil

--- ali/fetch_comment.php
+++ ali/fetch_comment.php
@@ -20,17 +20,17 @@ foreach($result as $row)
 {
     //var_dump($row);
     $pseudo = $row['pseudo'];
-$output .= "
-<div class='col-sm-9'>
-<form method='get' action='info_profil.php'>
-    <div class='well'>By <b><input type='text' name='pseudo' style='display: none;' value='$pseudo'/><button type='submit'>$pseudo</button></b> on <i>".$row['tweet_date']."</i>
-</form>
-    <br><br>
-    ".$row['content_tweet']."
-    </div>
-    <button class='btn'><span class='glyphicon glyphicon-thumbs-up'></button>
-    <button class='btn'><span class='glyphicon glyphicon-retweet'></button><br>
-</div>"; 
+    $output .= "
+    <div class='col-sm-9'>
+    <form method='get' action='info_profil.php'>
+        <div class='well'>By <b><input type='text' name='pseudo' style='display: none;' value='$pseudo'/><button type='submit'>$pseudo</button></b> on <i>".$row['tweet_date']."</i>
+    </form>
+        <br><br>
+        ".$row['content_tweet']."
+        </div>
+        <button class='btn'><span class='glyphicon glyphicon-thumbs-up'></button>
+        <button class='btn'><span class='glyphicon glyphicon-retweet'></button><br>
+    </div>"; 
 //$output .= get_reply_comment($connect);
 //var_dump($output);
 }
